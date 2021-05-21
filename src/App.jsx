bd76ba8f652c9731ff4313c210d8d1ea17d2bc13
Quarcoo-arc <53879894+Quarcoo-arc@users.noreply.github.com>
2021-05-21 13:55:30 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Footer />
+      <Note />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+var today = new Date();
+var currentYear = today.getFullYear();
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright 2021 </p>
+    </footer>
+  );
+}
+
+export default Footer;
